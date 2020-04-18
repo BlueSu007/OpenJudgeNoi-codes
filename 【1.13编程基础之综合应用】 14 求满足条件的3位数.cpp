527d790d0e339afd27d4ac21dc0378243db3674d
BlueSu007 <62138430+BlueSu007@users.noreply.github.com>
2020-04-18 08:37:07 +0800

    Create 【1.13编程基础之综合应用】 14 求满足条件的3位数.cpp

---
+++ 【1.13编程基础之综合应用】 14 求满足条件的3位数.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int x;
+	cin>>x;
+	
+	if(x==1)	cout<<100;
+	if(x==2)	cout<<121;
+	if(x==3)	cout<<144;
+	if(x==4)	cout<<225;
+	if(x==5)	cout<<400;
+	if(x==6)	cout<<441;
+	if(x==7)	cout<<484;
+	if(x==8)	cout<<676;
+	if(x==9)	cout<<900;
+	
+	return 0;
+}
